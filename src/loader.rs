94bc647449123654557fa783379fe115a0a4f5c1
Denis Papathanasiou <denis@papathanasiou.org>
2023-11-19 12:44:57 -0500

    Moved the web server and rss loader into their own modules, simplifying main

---
+++ src/loader.rs
@@ -0,0 +1,42 @@
+use crate::parser::{get_photos_from_feed, NewsPhoto};
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+use std::time::Duration;
+use tokio::time;
+
+pub type FeedDb = Arc<Mutex<HashMap<String, Vec<NewsPhoto>>>>;
+
+async fn fetch(db: FeedDb) {
+    let feeds = vec![
+        "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml",
+        "https://www.politico.com/rss/politicopicks.xml",
+        "https://www.wired.com/feed/rss",
+        "https://www.france24.com/en/rss",
+        "https://www.japantimes.co.jp/feed/",
+        "https://aeon.co/feed.rss",
+        "https://api.quantamagazine.org/feed/",
+    ];
+
+    for feed in feeds {
+        let photos = get_photos_from_feed(feed).await;
+        if let Ok(mut hash) = db.lock() {
+            hash.insert(feed.to_string(), photos.clone());
+        } else {
+            println!("could not obtain lock") // TODO: log error
+        }
+    }
+}
+
+pub async fn background(db: FeedDb, interval: u64) {
+    // load the FeedDb in the background, once at the given internal (in seconds)
+    let mut interval = time::interval(Duration::from_secs(interval));
+
+    loop {
+        let db = db.clone();
+        tokio::spawn(async move {
+            fetch(db).await;
+        });
+
+        interval.tick().await;
+    }
+}
---
+++ src/server.rs
@@ -0,0 +1,54 @@
+use crate::loader::FeedDb;
+use actix_web::http::header::ContentType;
+use actix_web::HttpResponse;
+use actix_web::{dev::Server, web, App, HttpServer};
+use std::net::TcpListener;
+
+pub struct AppState {
+    pub feeds: FeedDb,
+    pub next_size: usize,
+}
+
+async fn get_next(offset: web::Path<String>, state: web::Data<AppState>) -> HttpResponse {
+    let start: usize = match offset.to_string().parse() {
+        Ok(i) => i,
+        Err(_) => 0,
+    };
+    let stop = &state.clone().next_size;
+
+    let mut body = String::new();
+    let feeds = &state.clone().feeds;
+    if let Ok(db) = feeds.lock() {
+        let photos = db.values().flatten().collect::<Vec<_>>();
+        let subset = &photos[start..=(start + stop)];
+        body = subset
+            .iter()
+            .map(|photo| photo.as_json())
+            .filter(|p| p.is_ok())
+            .map(|p| p.unwrap())
+            .collect::<Vec<_>>()
+            .join(",");
+    }
+
+    HttpResponse::Ok()
+        .content_type(ContentType::json())
+        .body(format!("[{body}]"))
+}
+
+pub fn run(listener: TcpListener, db: FeedDb, next_size: usize) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(AppState {
+                feeds: db.clone(),
+                next_size,
+            }))
+            .service(
+                web::scope("/api")
+                    .service(web::resource("/next/{offset}").route(web::get().to(get_next))),
+            )
+    })
+    .listen(listener)?
+    .run();
+
+    Ok(server)
+}
