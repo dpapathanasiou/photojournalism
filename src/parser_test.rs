69edcfdade7f0c98b566eec78d3d2fb2a34fe836
Denis Papathanasiou <denis@papathanasiou.org>
2023-11-24 11:56:45 -0500

    Additional test for feeds using Dublin Core

--- src/parser_test.rs
+++ src/parser_test.rs
@@ -26,7 +26,7 @@ fn parser_can_find_images_in_cdata() {
 
     While this is non-standard, the parser is capable of finding them, which
     is what this test confirms.
-    */
+     */
 
     let aeon = load_fixture("aeon.xml".to_string());
     assert!(aeon.is_some());
@@ -41,10 +41,10 @@ fn parser_can_find_images_in_cdata() {
 #[test]
 fn parser_ignores_feeds_without_images() {
     /*
-    Alas, the BBC feed does not contain any images (which is perfectly fine
+    The BBC feed does not contain any images (which is perfectly fine
     according to the RSS spec, it's just that we cannot list it among the
     feeds to collect here).
-    */
+     */
 
     let bbc = load_fixture("bbc.xml".to_string());
     assert!(bbc.is_some());
@@ -54,4 +54,35 @@ fn parser_ignores_feeds_without_images() {
 
     let results = get_photos(channel.unwrap());
     assert_eq!(results.len(), 0);
+}
+
+#[test]
+fn parser_finds_dublin_core_correctly() {
+    /*
+    Quanta Magazine's feed uses Dublin Core for photo credits, inside CDATA blocks,
+    so this test confirms that the parser can get to underlying strings correctly.
+     */
+
+    let quanta = load_fixture("quanta.xml".to_string());
+    assert!(quanta.is_some());
+
+    let channel = Channel::from_str(&quanta.unwrap());
+    assert!(channel.is_ok());
+
+    let results = get_photos(channel.unwrap());
+    assert_eq!(results.len(), 5);
+
+    let expected_credits = vec![
+        "Patrick Honner",
+        "Yasemin Saplakoglu",
+        "Madison Goldberg",
+        "Alex Stone",
+        "Stephen Ornes",
+    ];
+    let actual_credits: Vec<String> = results
+        .iter()
+        .filter(|photo| photo.credit.is_some())
+        .map(|photo| photo.credit.clone().unwrap())
+        .collect();
+    assert_eq!(actual_credits, expected_credits);
 }
