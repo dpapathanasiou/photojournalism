d22564b1459888f6065760648a21371cc76163b2
Denis Papathanasiou <denis@papathanasiou.org>
2023-11-24 12:45:08 -0500

    Parser test of a well-produced feed

--- src/parser_test.rs
+++ src/parser_test.rs
@@ -144,3 +144,55 @@ fn parser_finds_image_content_when_given_both_thumbnails_and_content() {
         .collect();
     assert_eq!(actual_images, expected_images);
 }
+
+#[test]
+fn parser_finds_image_description_when_available() {
+    /*
+    The NY Times feed is a good example of well-executed content, which follows
+    the RSS spec correctly.
+
+    In particular, they add a description specific to the image (<media:description>),
+    which is separate and distinct from the item description (<description>).
+
+    This test confirms the parser always picks the former, when given both.
+     */
+
+    let feed = load_fixture("nytimes.xml".to_string());
+    assert!(feed.is_some());
+
+    let channel = Channel::from_str(&feed.unwrap());
+    assert!(channel.is_ok());
+
+    let results = get_photos(channel.unwrap());
+    assert_eq!(results.len(), 20);
+
+    let expected_descriptions = vec![
+        "Gaza City. Hamas seized control of Gaza in 2007, after winning legislative elections the previous year.", 
+        "Dr. Benjamin Neel was fired over his pro-Israel postings on social media. Now he is suing his employer, NYU Langone Health.", 
+        "Much of Americans’ spending starts well before the day after Thanksgiving, particularly online.", 
+        "Nordstrom’s visual team tackles decorating stores beginning on the Monday evening before Thanksgiving.", 
+        "Navigating the Black Friday Travels Sales and Deals", 
+        "A family of migrants from China in Texas after surrendering to Border Patrol in April.", 
+        "Riot police in Dublin on Thursday.", 
+        "Nikki Haley still trails former President Donald J. Trump by a wide margin in polls, but she has gained ground on Gov. Ron DeSantis of Florida, who has held the No. 2 spot in national surveys all year.", 
+        "Cliff Albright, co-founder and executive director of the Black Voters Matter Fund, said donors and party leaders were weighing heavier investments in swing states like Michigan, Pennsylvania and Wisconsin.", 
+        "A shop in Buenos Aires, where prices are often written in chalk to allow for frequent increases.", 
+        "Oscar Pistorius arriving at the Pretoria High Court in 2016. A parole board granted Mr. Pistorius’s petition for parole on the basis that he had served half of his 15-year sentence.", 
+        "Boys from the Leones baseball team at the Hermes Barros Cabas baseball stadium in Bogotá.", 
+        "George Santos and the Very Good Reason People Lie About Nonsense", 
+        "Patagonia’s Yvon Chouinard On the High Stakes of Low Quality", 
+        "Ayana Elizabeth Johnson.", 
+        "Investigators said the car was a Bentley that sped toward the bridge, hit a median and went airborne.", 
+        "Mayor Dan Carter at city hall in Oshawa, Ontario. Homeless and addicted to drugs from his teenage years until he was 31, he was fired from more jobs than he could remember.", 
+        "Davida Wynn became infected with the coronavirus in May 2020 when she was a nurse on a Covid unit, and became so ill she was put into a medically induced coma for six weeks.", 
+        "Monetochka performing in Zurich this month.", 
+        "Decked out shelves at John Derian’s new holiday shop, which was set up inside a store normally used as a furniture showroom.",
+    ];
+
+    let actual_descriptions: Vec<String> = results
+        .iter()
+        .filter(|photo| photo.description.is_some())
+        .map(|photo| photo.description.clone().unwrap())
+        .collect();
+    assert_eq!(actual_descriptions, expected_descriptions);
+}
